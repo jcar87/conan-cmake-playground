556b608b9c1bbd0b5c3f1d65e34bc1ea0551b344
James <james@conan.io>
2025-02-12 10:52:32 +0100

    Automate readelf and verbosity (#3)
    * wip
    
    * more automation
    
    * add dumpbin in Windows
    
    * dependents in windows
    
    * working with new PR to CMakeDeps incubating

--- projects/squirtle/conanfile.py
+++ projects/squirtle/conanfile.py
@@ -1,12 +1,15 @@
+import os
+
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.microsoft import VCVars
 
 
 class pikachuRecipe(ConanFile):
     name = "squirtle"
     version = "1.0"
     package_type = "application"
-
+    exports_sources = "CMakeLists.txt", "*.cpp"
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
@@ -20,6 +23,18 @@ class pikachuRecipe(ConanFile):
         deps.generate()
         tc = CMakeToolchain(self)
         tc.generate()
+        VCVars(self).generate(scope="run")
 
     def requirements(self):
         self.requires("bulbasaur/1.0")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        exe = os.path.join(self.cpp.build.bindir, "hello")
+        self.run(exe, env="conanrun")
+        if self.settings.os == "Linux":
+            self.run(f"readelf -d {exe}")
+        if self.settings.os == "Windows":
+            self.run(f"dumpbin /DEPENDENTS {exe}.exe", env="conanrun")
